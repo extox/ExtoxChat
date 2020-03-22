c3c5f873711e5901b5903932c99b70ece4505d14
jincrom <jincrom@gmail.com>
2020-03-22 11:08:56 +0900

    초기 커밋.

---
+++ ExtoxChat_Server/Hubs/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExtoxChat_Server.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ExtoxChat_Server.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task NewMessage(Message msg)
+        {
+            await Clients.All.SendAsync("MessageReceived", msg);
+        }
+    }
+}
